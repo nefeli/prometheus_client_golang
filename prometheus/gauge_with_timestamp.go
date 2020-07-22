8f1aa1850f0f3289a150d60e967159b5d708fa83
Shinae Woo <shinae2012@gmail.com>
2020-07-22 02:00:37 +0000

    ..

--- prometheus/gauge_with_timestamp.go
+++ prometheus/gauge_with_timestamp.go
@@ -10,24 +10,14 @@ import (
 )
 
 type GaugeWithTimestamp interface {
-	Metric
-	Collector
 
 	// SetTimestamp sets the timestamp as given value
 	SetWithTimestamp(float64, float64)
 }
 
 type gaugeWithTimestamp struct {
-	// valBits contains the bits of the represented float64 value. It has
-	// to go first in the struct to guarantee alignment for atomic
-	// operations.  http://golang.org/pkg/sync/atomic/#pkg-note-BUG
-	valBits uint64
+	Gauge
 	time    float64
-
-	selfCollector
-
-	desc       *Desc
-	labelPairs []*dto.LabelPair
 }
 
 func NewGaugeWithTimestamp(opts GaugeOpts) GaugeWithTimestamp {
@@ -37,23 +27,22 @@ func NewGaugeWithTimestamp(opts GaugeOpts) GaugeWithTimestamp {
 		nil,
 		opts.ConstLabels,
 	)
-	result := &gaugeWithTimestamp{desc: desc, labelPairs: desc.constLabelPairs}
-	result.init(result) // Init self-collection.
+	g := NewGauge(opts)
+	result := &gaugeWithTimestamp{Gauge: g}
 	return result
 }
 
 func (g *gaugeWithTimestamp) Desc() *Desc {
-	return g.desc
+	return g.Gauge.Desc()
 }
 
 func (g *gaugeWithTimestamp) SetWithTimestamp(val float64, time float64) {
-	atomic.StoreUint64(&g.valBits, math.Float64bits(val))
+	g.Gauge.Set(val)
 	g.time = time
 }
 
 func (g *gaugeWithTimestamp) Write(out *dto.Metric) error {
-	val := math.Float64frombits(atomic.LoadUint64(&g.valBits))
-	m := populateMetric(GaugeValue, val, g.labelPairs, nil, out)
+	m := g.Gauge.Write(out)
 	out.TimestampMs = proto.Int64(int64(g.time * 1000))
 	return m
 }
